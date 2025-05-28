08c984a8e2c654d97755e12a1184a633e1b5f73c
Dmsooja <dev@sooja.fr>
2025-05-28 18:55:57 +0200

    Create stories slice

--- slices/Stories/index.tsx
+++ slices/Stories/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 
 /**
  * Props for `TestimonialCardsWithImage`.
@@ -15,39 +16,33 @@ const TestimonialCardsWithImage: FC<TestimonialCardsWithImageProps> = ({
   slice,
 }) => {
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for testimonial_cards_with_image (variation:{" "}
-      {slice.variation}) slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use Prismic MCP with your code editor
-       *
-       * Get AI-powered help to build your slice components — based on your actual model.
-       *
-       * ▶️ Setup:
-       * 1. Add a new MCP Server in your code editor:
-       *
-       * {
-       *   "mcpServers": {
-       *     "Prismic MCP": {
-       *       "command": "npx",
-       *       "args": ["-y", "@prismicio/mcp-server"]
-       *     }
-       *   }
-       * }
-       *
-       * 2. Select Claude 3.7 Sonnet (recommended for optimal output)
-       *
-       * ✅ Then open your slice file and ask your code editor:
-       *    "Code this slice"
-       *
-       * Your code editor reads your slice model and helps you code faster ⚡
-       * 📚 Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
-       */}
+    <section className="py-12 px-4 bg-white">
+      <div className="container mx-auto max-w-7xl">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          {slice.primary.cards.map((item, idx) => (
+            <PrismicNextLink
+              field={item.link}
+              key={idx}
+              className="relative h-96 bg-green-500 overflow-hidden transition duration-300"
+            >
+              <PrismicNextImage
+                field={item.image}
+                className="absolute inset-0 object-cover"
+                width={624}
+                height={384}
+              />
+              <div className="absolute w-full z-10 p-4 h-fit bottom-0 flex flex-col justify-center bg-white/80 hover:bg-white/90">
+                <div className="text-3xl font-semibold mb-4 text-primary-green">
+                  <PrismicRichText field={item.headline} />
+                </div>
+                <div className="text-xl leading-relaxed text-font-gray">
+                  <PrismicRichText field={item.description} />
+                </div>
+              </div>
+            </PrismicNextLink>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
