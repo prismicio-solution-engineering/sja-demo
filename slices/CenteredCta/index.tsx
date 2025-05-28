009ffbd1f4938713a91d63f49e2cd1bb21f70fbf
Dmsooja <dev@sooja.fr>
2025-05-28 18:55:07 +0200

    Create CTA slice

--- slices/CenteredCta/index.tsx
+++ slices/CenteredCta/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import { PrismicNextLink } from "@prismicio/next";
 
 /**
  * Props for `CenteredCta`.
@@ -12,39 +13,18 @@ export type CenteredCtaProps = SliceComponentProps<Content.CenteredCtaSlice>;
  */
 const CenteredCta: FC<CenteredCtaProps> = ({ slice }) => {
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for centered_cta (variation: {slice.variation})
-      slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use Prismic MCP with your code editor
-       *
-       * Get AI-powered help to build your slice components — based on your actual model.
-       *
-       * ▶️ Setup:
-       * 1. Add a new MCP Server in your code editor:
-       *
-       * {
-       *   "mcpServers": {
-       *     "Prismic MCP": {
-       *       "command": "npx",
-       *       "args": ["-y", "@prismicio/mcp-server"]
-       *     }
-       *   }
-       * }
-       *
-       * 2. Select Claude 3.7 Sonnet (recommended for optimal output)
-       *
-       * ✅ Then open your slice file and ask your code editor:
-       *    "Code this slice"
-       *
-       * Your code editor reads your slice model and helps you code faster ⚡
-       * 📚 Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
-       */}
+    <section className="w-full p-8 bg-yellow">
+      <div className="mx-auto text-center">
+        <div className="font-medium text-sm md:text-base">
+          <PrismicNextLink
+            field={slice.primary.button_link}
+            className="p-4 text-base text-white"
+            style={{
+              backgroundColor: `${slice.primary.button_background_color}`,
+            }}
+          />
+        </div>
+      </div>
     </section>
   );
 };
