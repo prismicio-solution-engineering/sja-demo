18e17f71d764d8fd70e0dcb9f369c12744cec3e1
Dmsooja <dev@sooja.fr>
2025-05-28 16:55:08 +0200

    Create hero

--- slices/HeroWithBackgroundImage/index.tsx
+++ slices/HeroWithBackgroundImage/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { PrismicNextImage } from "@prismicio/next";
 
 /**
  * Props for `HeroWithBackgroundImage`.
@@ -15,39 +16,35 @@ const HeroWithBackgroundImage: FC<HeroWithBackgroundImageProps> = ({
   slice,
 }) => {
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for hero_with_background_image (variation:{" "}
-      {slice.variation}) slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use Prismic MCP with your code editor
-       *
-       * Get AI-powered help to build your slice components — based on your actual model.
-       *
-       * ▶️ Setup:
-       * 1. Add a new MCP Server in your code editor:
-       *
-       * {
-       *   "mcpServers": {
-       *     "Prismic MCP": {
-       *       "command": "npx",
-       *       "args": ["-y", "@prismicio/mcp-server"]
-       *     }
-       *   }
-       * }
-       *
-       * 2. Select Claude 3.7 Sonnet (recommended for optimal output)
-       *
-       * ✅ Then open your slice file and ask your code editor:
-       *    "Code this slice"
-       *
-       * Your code editor reads your slice model and helps you code faster ⚡
-       * 📚 Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
-       */}
+    <section className="relative w-full overflow-hidden">
+      <PrismicNextImage
+        field={slice.primary.background_image.mobile}
+        height={401}
+        className="absolute w-full inset-0 object-cover md:hidden"
+      />
+      <PrismicNextImage
+        field={slice.primary.background_image.tablet}
+        height={473}
+        className="absolute w-full inset-0 object-cover hidden md:block lg:hidden"
+      />
+      <PrismicNextImage
+        field={slice.primary.background_image}
+        height={508}
+        className="absolute w-full inset-0 object-cover hidden lg:block"
+      />
+      {/* Content Container */}
+      <div className="relative z-10 flex items-center lg:mt-20">
+        <div className="container mx-auto px-4 md:px-6 lg:px-8 h-fit">
+          <div className="max-w-2xl">
+            <div className="md:text-3xl lg:text-4xl font-semibold text-6xl text-font-gray bg-yellow px-10 py-2 md:px-6 md:py-3">
+              <PrismicRichText field={slice.primary.title} />
+            </div>
+            <div className="text-white p-10 font-medium text-3xl md:text-xl lg:text-2xl leading-relaxed bg-black/60 w-3/4">
+              <PrismicRichText field={slice.primary.description} />
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
